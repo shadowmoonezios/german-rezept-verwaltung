1551d2a7e34cb02984497bf6ba3bacb005af281f
shadowmoonezios <agushifelde@hotmail.com>
2025-09-24 17:06:15 +0300

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Verbindungs-URL
+const dbURI = 'mongodb://localhost:27017/rezeptverwaltung';
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Datenbank verbunden'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('Willkommen zur Rezept Verwaltung!');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server läuft auf Port ${PORT}`);
+});
